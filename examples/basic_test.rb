9bbeabe6bb4ec29c726ebd6d548d542dea25813f
Samuel Tonini <tonini.samuel@gmail.com>
2011-08-19 14:06:53 +0200

    add more assertion methods, #assert_kind_of, #assert_instance_of and #assert_respond_to

--- examples/basic_test.rb
+++ examples/basic_test.rb
@@ -39,4 +39,43 @@ class BasicTest
     assert_nil box
   end
 
+  test "assert_kind_of failed" do
+    string = 23
+
+    assert_kind_of String, string
+  end
+
+  test "assert_kind_of passed" do
+    fixnum = 11
+
+    assert_kind_of Fixnum, fixnum
+  end
+
+  test "assert_instance_of failed" do
+    float = 120e2
+
+    assert_instance_of Fixnum, float
+  end
+
+  test "assert_instance_of passed" do
+    bignum = 99999999999999999999999
+
+    assert_instance_of Bignum, bignum
+  end
+
+  test "assert_respond_to failed" do
+    class Responder
+      def hello; end
+    end
+
+    assert_respond_to Responder.new, :bye
+  end
+
+  test "assert_respond_to passed" do
+    class Responder
+      def hello; end
+    end
+
+    assert_respond_to Responder.new, :hello
+  end
 end
--- lib/overseer/assertions.rb
+++ lib/overseer/assertions.rb
@@ -20,5 +20,20 @@ module Overseer
       message ||= "Expected \"#{obj}\" to be nil"
       assert(obj.nil?, message)
     end
+
+    def assert_kind_of(clazz, obj, message=nil)
+      message ||= "Expected \"#{obj.inspect}\" to be a kind of \"#{clazz}\", not \"#{obj.class}\""
+      assert(obj.kind_of?(clazz), message)
+    end
+
+    def assert_instance_of(clazz, obj, message=nil)
+      message ||= "Expected \"#{obj.inspect}\" to be an instance of \"#{clazz}\", not \"#{obj.class}\""
+      assert(obj.instance_of?(clazz), message)
+    end
+
+    def assert_respond_to(obj, method, message=nil)
+      message ||= "Expected \"#{obj.inspect}\" to respond to \"##{method}\""
+      assert(obj.respond_to?(method), message)
+    end
   end
 end
