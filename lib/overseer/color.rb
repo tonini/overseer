b01f56dc0c8c0b74a3e602f4c911bbed950b5c3f
Samuel Tonini <samuel.tonini@garaio.com>
2011-08-19 11:37:49 +0200

    just generate methods for the needed colors

--- lib/overseer/color.rb
+++ lib/overseer/color.rb
@@ -3,22 +3,11 @@ module Overseer
     extend self
 
     COLORS = {
-      :black             =>   "\e[30m",
       :red               =>   "\e[31m",
       :green             =>   "\e[32m",
-      :yellow            =>   "\e[33m",
-      :blue              =>   "\e[34m",
-      :magenta           =>   "\e[35m",
-      :cyan              =>   "\e[36m",
-      :white             =>   "\e[37m",
       :bright_black      =>   "\e[90m",
       :bright_red        =>   "\e[91m",
       :bright_green      =>   "\e[92m",
-      :bright_yellow     =>   "\e[93m",
-      :bright_blue       =>   "\e[94m",
-      :bright_magenta    =>   "\e[95m",
-      :bright_cyan       =>   "\e[96m",
-      :bright_white      =>   "\e[97m"
     }
 
     COLORS.each do |color, ansi_code|
